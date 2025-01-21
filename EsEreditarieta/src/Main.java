6eee6e829dd2d14864e703b96ffbf493dd009835
Alexander <alexanderiba005@gmail.com>
2025-01-21 12:28:27 +0100

    finito

--- EsEreditarieta/src/Main.java
+++ EsEreditarieta/src/Main.java
@@ -1,5 +1,20 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // creazione oggetto impiegato
+
+        Impiegato impiegato = new Impiegato("Gianmarco", "Baldo", LocalDate.now(), "321", 2000, LocalDate.now() );
+
+        System.out.println(impiegato);
+        impiegato.riceviAumento(500);
+        impiegato.saluta();
+
+        Professionista professionista = new Professionista("Maria", "Santa", LocalDate.now(), "1243242311", LocalDate.now(), 2000);
+
+        System.out.println(professionista);
+        professionista.fattura(3000);
+        professionista.saluta();
+
     }
 }
