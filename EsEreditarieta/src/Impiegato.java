33f023cde0e234aca9326a7f3eeb7048c07e061e
NicoloGiuliani <nigulinu@gmail.com>
2025-01-21 11:58:00 +0100

    Fino impiegato

--- EsEreditarieta/src/Impiegato.java
+++ EsEreditarieta/src/Impiegato.java
@@ -1,3 +1,46 @@
-public class Impiegato {
+import java.time.LocalDate;
 
+public class Impiegato extends Persona {
+    
+    private String nrTesserino;
+    private Integer Stipendio;
+    private LocalDate dataAssunzione;
+    
+    //costruttore
+    public Impiegato(String nome, String cognome, LocalDate dataNascita, String nrTesserino, Integer stipendio,
+            LocalDate dataAssunzione) {
+        super(nome, cognome, dataNascita);
+        this.nrTesserino = nrTesserino;
+        Stipendio = stipendio;
+        this.dataAssunzione = dataAssunzione;
+    }
+
+    //metodi
+    public void riceviAumento(int aumento) {
+        if (aumento<=0) {
+            System.out.println("Aumento non valido");
+            return;
+        }
+        Stipendio += aumento;
+    }
+    
+    //getters
+    public String getNrTesserino() {
+        return nrTesserino;
+    }
+
+    public Integer getStipendio() {
+        return Stipendio;
+    }
+
+    public LocalDate getDataAssunzione() {
+        return dataAssunzione;
+    }
+
+    @Override
+    public String toString() {
+        return "Impiegato [nrTesserino=" + nrTesserino + ", Stipendio=" + Stipendio + ", dataAssunzione="
+                + dataAssunzione + ", Nome=" + getNome() + ", Cognome=" + getCognome() + ", DataNascita="
+                + getDataNascita() + "]";
+    }    
 }
--- EsEreditarieta/src/Persona.java
+++ EsEreditarieta/src/Persona.java
@@ -1,3 +1,32 @@
-public class Persona {
+import java.time.LocalDate;
 
+public class Persona {
+    
+    private String nome, cognome;
+    private LocalDate dataNascita;
+    
+    //costruttore
+    public Persona(String nome, String cognome, LocalDate dataNascita) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.dataNascita = dataNascita;
+    }
+
+    //metodi
+    public void saluta() {
+        System.out.println("Ciao, sono " + nome + " " + cognome + " e sono nato il " + dataNascita);
+    }
+
+    //getters
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public LocalDate getDataNascita() {
+        return dataNascita;
+    }
 }
